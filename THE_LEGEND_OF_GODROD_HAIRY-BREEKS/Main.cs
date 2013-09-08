d87bcb96cfd19870b73214558ea655335ac878ec
kairos <kairosfractes@gmail.com>
2013-09-08 00:22:30 -0400

    more stats
    lots more stats, currently a problem/bug with rerolling stats during
    confirmation of class

--- THE_LEGEND_OF_GODROD_HAIRY-BREEKS/Main.cs
+++ THE_LEGEND_OF_GODROD_HAIRY-BREEKS/Main.cs
@@ -7,37 +7,35 @@ namespace THE_LEGEND_OF_GODROD_HAIRYBREEKS
 	{
 		public static void Main ()
 		{
+			string classname;
 			string playername;
-			string playerclass;
 			bool classchosen = false;
 			bool characterchosen = false;
-			string heroreroll;
 			Hero player = new Warrior ("satan");
 			while (!characterchosen) {
 				Console.WriteLine ("WHAT IS YOUR NAME, HERO?");
 				playername = Console.ReadLine ();
 				while (!classchosen) {
+					Console.WriteLine ("Being an individual of determination and skill, you can pick up pretty much anything and be good at it." +
+						"\nHowever, time and lifestyle changes have forced you to pick a specific skillset.");
+					Console.WriteLine ("=============================================");
 					Console.WriteLine ("PICK YOUR CLASS: WARRIOR, MAGE, THIEF (W,M,T)");
-					playerclass = Console.ReadLine ();
-					switch (playerclass) {
-					case "W":
+					switch (Console.ReadKey ().Key) {
+					case ConsoleKey.W:
 						player = new Warrior (playername);
 						classchosen = true;
+						classname = "warrior";
 						break;
-					case "M":
+					case ConsoleKey.M:
 						player = new Mage (playername);
 						classchosen = true;
+						classname = "mage";
 						break;
-					case "T":
+					case ConsoleKey.T:
 						player = new Thief (playername);
 						classchosen = true;
+						classname = "thief";
 						break;
-					case "w":
-						goto case "W";
-					case "m":
-						goto case "M";
-					case "t":
-						goto case "T";
 					default:
 						Console.WriteLine ("DAS NOT A CALSS SIR");
 						break;
@@ -48,52 +46,72 @@ namespace THE_LEGEND_OF_GODROD_HAIRYBREEKS
 				Console.WriteLine ("PLAYER STATS");
 				Console.WriteLine ("============");
 				Console.WriteLine ("Name: {0}", player.myname);
-				Console.WriteLine ("Strength: {0}", player.stat_strength);
-				Console.WriteLine ("Intelligence: {0}", player.stat_intelligence);
+				Console.WriteLine ("Health: {0}", player.health);
+				Console.WriteLine ("Magicka: {0}", player.magicka);
+				Console.WriteLine ("============");
 				Console.WriteLine ("Agility: {0}", player.stat_agility);
+				Console.WriteLine ("Endurance: {0}", player.stat_endurance);
+				Console.WriteLine ("Intelligence: {0}", player.stat_intelligence);
+				Console.WriteLine ("Luck: {0}", player.stat_luck);
+				Console.WriteLine ("Personality: {0}", player.stat_personality);
+				Console.WriteLine ("Speed: {0}", player.stat_speed);
+				Console.WriteLine ("Strength: {0}", player.stat_strength);
+				Console.WriteLine ("Willpower: {0}", player.stat_willpower);
+				Console.WriteLine ("============");
+				Console.WriteLine ("Block: {0}", player.skill_block);
+				Console.WriteLine ("Heavy Weapon: {0}", player.skill_heavyweap);
+				Console.WriteLine ("Light Weapon: {0}", player.skill_lightweap);
+				Console.WriteLine ("Destruction: {0}", player.skill_destruction);
+				Console.WriteLine ("Restoration: {0}", player.skill_restoration);
+				Console.WriteLine ("Alteration: {0}", player.skill_alteration);
+				Console.WriteLine ("============");
 				Console.WriteLine ("ARE YOU THE HERO YOU SAY YOU ARE? (y/n)");
-				heroreroll = Console.ReadLine ();
-				switch (heroreroll) {
-				case "n":
+				switch (Console.ReadKey ().Key) {
+				case ConsoleKey.N:
 					Console.WriteLine ("WHO ARE YOU, REALLY?");
 					break;
-				case "N":
-					Console.WriteLine ("WHO ARE YOU, REALLY?");
-					break;
-				case "r":
-					player = new Warrior (playername);
-					goto rolled;
-				default:
-					Console.WriteLine ("THEN LET US BEGIN...");
-					characterchosen = true;
-					break;
+				case ConsoleKey.R:
+					if (classname == "warrior") {
+						player = new Warrior (playername);
+						goto rolled;
+					}
+					if (classname == "mage") {
+						player = new Mage (playername);
+						goto rolled;
+					}
+					if (classname == "thief") {
+						player = new Thief (playername);
+						goto rolled;
+					}
 				}
 			}
 			Console.Clear ();
-			Console.WriteLine ("You begin in the dungeons of Detroit, in a cell only big enough for you and the eight corpses you're being smothered with.\n" +
-				"However, fate has shone upon you, for Lord Godrod Hairy-Breeks summons you to Castle Frankenbaum for an audience.\n" +
-				"'You are one lucky peasant, {0}, for we have had to place another corpse in the dungeon cell you occupied. As we have no other cells,\n" +
-				"you will be my personal courier and knight. As of now, I have no tasks for you to complete. Rather, you should gather experience for yourself\n" +
-				"out in the world. Find princesses to fuck, merchants to hustle out of their money, and horses to practice sexual intercourse with.\n" +
-				"Basically, get the fuck out of my castle.\n'" +
+			Console.WriteLine ("You begin in the dungeons of Detroit, in a cell only big enough for you and the eight corpses you're being smothered with. " +
+				"However, fate has shone upon you, for Lord Godrod Hairy-Breeks summons you to Castle Frankenbaum for an audience. " +
+				"'You are one lucky peasant {0}, for we have had to place another corpse in the dungeon cell you occupied. Since we have no other cells, " +
+				"you will be my personal courier and knight. As of now, I have no tasks for you to complete. Rather, you should gather experience for yourself " +
+				"out in the world. Find princesses to fuck, merchants to hustle out of their money, or animals to practice sexual intercourse with!\n" +
+				"Anyways, get the fuck out of my castle.\n'" +
 				"==========================================\n" +
 				"1. Exit the castle doors\n" +
 				"2. Stare at Lord Godrod\n" +
 				"3. Say 'ok, mom'", player.myname);
-			int answer = Convert.ToInt32 (Console.ReadLine ());
-			switch (answer) {
-			case 1:
+			switch (Console.ReadKey(false).Key) {
+			case ConsoleKey.D1:
+				Console.WriteLine ("case 1");
 				goto default;
-			case 2:
+			case ConsoleKey.D2:
+				Console.WriteLine ("case 2");
 				goto default;
-			case 3:
+			case ConsoleKey.D3:
+				Console.WriteLine ("case 3");
 				goto default;
 			default:
 				Console.WriteLine ("Godrod Hairy-Breeks: 'YOU DARE TO BE SNIPPY WITH ME?! YOU WILL BE TAUGHT BY MY FAT WIENER WHAT RESPECT IS! PREPARE FOR THE ANAL RAVAGING OF YOUR LIFETIME, PEASANT!'\n" +
 					"Lord Godrod tears off his leathers in fury, and begins to storm towards you with his dong dragging across the carpeted floor...");
 				player.Die ();
 				break;
+				}
 			}
 		}
 	}
-}
--- THE_LEGEND_OF_GODROD_HAIRY-BREEKS/Rolling.cs
+++ THE_LEGEND_OF_GODROD_HAIRY-BREEKS/Rolling.cs
@@ -5,7 +5,8 @@ namespace THE_LEGEND_OF_GODROD_HAIRYBREEKS
 	public abstract class Hero
 	{
 		protected Random RND = new Random ();
-		//stats
+		//
+		public string myname;
 		public int health;
 		public int magicka;
 		public int encumbrance;
@@ -17,7 +18,14 @@ namespace THE_LEGEND_OF_GODROD_HAIRYBREEKS
 		public int stat_speed;
 		public int stat_strength;
 		public int stat_willpower;
-		public string myname;
+		//
+		public int skill_block;
+		public int skill_heavyweap;
+		public int skill_lightweap;
+		public int skill_restoration;
+		public int skill_destruction;
+		public int skill_alteration;
+		//
 		public Hero ()
 		{
 		}
@@ -31,10 +39,27 @@ namespace THE_LEGEND_OF_GODROD_HAIRYBREEKS
 	{
 		public Warrior (string name)
 		{
-			stat_strength = RND.Next (8, 17);
-			stat_intelligence = RND.Next (2, 9);
-			stat_agility = RND.Next (2, 9);
 			myname = name;
+			health = 150;
+			magicka = 50;
+			encumbrance = 200;
+			//major stats
+			stat_endurance = 20;
+			stat_strength = 20;
+			skill_heavyweap = 20;
+			//minor stats
+			stat_personality = 10;
+			stat_willpower = 10;
+			skill_block = 10;
+			skill_lightweap = 10;
+			skill_restoration = 10;
+			//other stats
+			stat_agility = 5;
+			stat_intelligence = 5;
+			stat_luck = 5;
+			stat_speed = 5;
+			skill_destruction = 5;
+			skill_alteration = 5;
 		}
 	}
 
@@ -42,10 +67,27 @@ namespace THE_LEGEND_OF_GODROD_HAIRYBREEKS
 	{
 		public Mage (string name)
 		{
-			stat_strength = RND.Next (2, 9);
-			stat_intelligence = RND.Next (8, 17);
-			stat_agility = RND.Next (2, 9);
 			myname = name;
+			health = 100;
+			magicka = 200;
+			encumbrance = 50;
+			//major stats
+			stat_intelligence = 20;
+			stat_willpower = 20;
+			skill_restoration = 15;
+			skill_destruction = 15;
+			skill_alteration = 15;
+			//minor stats
+			stat_agility = 10;
+			stat_endurance = 10;
+			stat_speed = 10;
+			skill_lightweap = 10;
+			//other stats
+			stat_luck = 5;
+			stat_personality = 5;
+			stat_strength = 5;
+			skill_block = 5;
+			skill_heavyweap = 5;
 		}
 	}
 
@@ -53,10 +95,27 @@ namespace THE_LEGEND_OF_GODROD_HAIRYBREEKS
 	{
 		public Thief (string name)
 		{
-			stat_strength = RND.Next (2, 9);
-			stat_intelligence = RND.Next (2, 9);
-			stat_agility = RND.Next (8, 17);
 			myname = name;
+			health = 100;
+			magicka = 50;
+			encumbrance = 100;
+			//major stats
+			stat_agility = 20;
+			stat_speed = 20;
+			skill_lightweap = 20;
+			//minor stats
+			stat_luck = 10;
+			stat_personality = 10;
+			stat_strength = 10;
+			skill_heavyweap = 10;
+			skill_alteration = 10;
+			//other stats
+			stat_endurance = 5;
+			stat_intelligence = 5;
+			stat_willpower = 5;
+			skill_block = 5;
+			skill_restoration = 5;
+			skill_destruction = 5;
 		}
 	}
 }
